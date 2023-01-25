f25a8dec0bc69b2e0699c56cf43649febff52bfa
ianMuchesia <ianmuchesia@yahoo.com>
2023-01-25 16:54:11 +0300

    base 64 troubles

--- server/app.js
+++ server/app.js
@@ -10,15 +10,14 @@ dotenv.config();
 const app = express()
 
 app.use(cors())
-
+app.use(express.json({ limit: '50mb'}))
 
 //middleware
 app.use('/api/v1/post', postRoutes)
 app.use('/api/v1/dalle', dalleRoutes)
 
-
 const port = 3000
-app.use(express.json({ limit: '50mb'}))
+
 
 
 app.get('/', async(req, res)=>{
