1b5237359dc9b23c9ad93984b5ff082cf2c1fdf1
ianMuchesia <ianmuchesia@yahoo.com>
2023-01-26 14:05:02 +0300

    final

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,5 @@
 import {  surpriseMePrompts } from "../assets/surpriseMe/index"
+import FileSaver from "file-saver"
 
 export const getRandomPrompt = (prompt) =>{
     const randomIndex = Math.floor(Math.random()*surpriseMePrompts.length);
@@ -8,4 +9,8 @@ export const getRandomPrompt = (prompt) =>{
     if(randomPrompt === prompt) return getRandomPrompt(prompt);
     
     return randomPrompt;
+}
+
+export async function downloadImage(_id, photo){
+    FileSaver.saveAs(photo, `${_id}.jpg`)
 }
